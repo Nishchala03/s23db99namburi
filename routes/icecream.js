20e94763349d6b86d3fa4d7e4b6673b8b8ec3f30
Nishchala Namburi <142998991+Nishchala03@users.noreply.github.com>
2023-11-15 20:01:27 -0600

    addded pug

--- routes/icecream.js
+++ routes/icecream.js
@@ -7,4 +7,8 @@ router.get('/', icecream_controlers.icecream_view_all_Page );
 router.get('/detail', icecream_controlers.icecream_view_one_Page);
 /* GET create icecream page */
 router.get('/create', icecream_controlers.icecream_create_Page);
+/* GET create update page */
+router.get('/update', icecream_controlers.icecream_update_Page);
+/* GET delete icecream page */
+router.get('/delete', icecream_controlers.icecream_delete_Page);
 module.exports = router;
