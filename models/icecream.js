1ce3e8b428603f9160ec7b3318054c23dba59193
Nishchala Namburi <142998991+Nishchala03@users.noreply.github.com>
2023-11-29 17:48:25 -0600

    updated pug files

--- models/icecream.js
+++ models/icecream.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose")
 const icecreamSchema = mongoose.Schema({
-flavor: String,
-type: String,
-rating: Number
+flavor:{
+    type: String,
+    required: true
+},
+type: {
+    type: String,
+    required: true
+},
+rating: {
+    type: Number,
+    required: true,
+    min:0,
+    max:5
+}
 })
 module.exports = mongoose.model("icecream",
 icecreamSchema)
