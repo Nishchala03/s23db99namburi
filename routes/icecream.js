7a290dd36f646db908974d974fe9cab888c820cb
Nishchala Namburi <142998991+Nishchala03@users.noreply.github.com>
2023-11-29 17:33:47 -0600

    Update icecream.js

--- routes/icecream.js
+++ routes/icecream.js
@@ -1,6 +1,12 @@
 var express = require('express');
 const icecream_controlers= require('../controllers/icecream');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET icecream */
 router.get('/', icecream_controlers.icecream_view_all_Page );
 /* GET detail icecream page */
@@ -8,7 +14,7 @@ router.get('/detail', icecream_controlers.icecream_view_one_Page);
 /* GET create icecream page */
 router.get('/create', icecream_controlers.icecream_create_Page);
 /* GET create update page */
-router.get('/update', icecream_controlers.icecream_update_Page);
+router.get('/update',secured, icecream_controlers.icecream_update_Page);
 /* GET delete icecream page */
 router.get('/delete', icecream_controlers.icecream_delete_Page);
 module.exports = router;
