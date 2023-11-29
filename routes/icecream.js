31ad911949f9330f2e6bfc248d449c6d72bb7a58
Nishchala Namburi <142998991+Nishchala03@users.noreply.github.com>
2023-11-29 17:50:54 -0600

    updated icecream

--- routes/icecream.js
+++ routes/icecream.js
@@ -10,11 +10,11 @@ const secured = (req, res, next) => {
 /* GET icecream */
 router.get('/', icecream_controlers.icecream_view_all_Page );
 /* GET detail icecream page */
-router.get('/detail', icecream_controlers.icecream_view_one_Page);
+router.get('/detail',secured, icecream_controlers.icecream_view_one_Page);
 /* GET create icecream page */
-router.get('/create', icecream_controlers.icecream_create_Page);
+router.get('/create',secured, icecream_controlers.icecream_create_Page);
 /* GET create update page */
 router.get('/update',secured, icecream_controlers.icecream_update_Page);
 /* GET delete icecream page */
-router.get('/delete', icecream_controlers.icecream_delete_Page);
+router.get('/delete',secured, icecream_controlers.icecream_delete_Page);
 module.exports = router;
